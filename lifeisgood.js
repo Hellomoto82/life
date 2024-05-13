d0d2bffb19f5939620eebbe54306baac16a23841
Hellomoto82 <topshoesmaroc@gmail.com>
2024-05-13 21:34:10 +0100

    lifeisgood.js

--- lifeisgood.js
+++ lifeisgood.js
@@ -297,7 +297,27 @@ const CasaregrPremiumButton = createButton("Casa Regroupement Premium", function
         }
       }
     }
+async function RemplirMission() {
+      var MissionElement;
+      var isVisibleMission;
+      for (var i = 1; i < 10; i++) {
+        MissionElement = document.querySelector(
+          '[aria-owns="Mission' + i + '_listbox"]'
+        );
+        if (MissionElement !== null) {
+          isVisibleMission = MissionElement.offsetParent !== null;
 
+          if (isVisibleMission) {
+            console.log("Mission " + i + " is visible");
+            var Location = $("#Mission" + i).data("kendoDropDownList");
+            Mission.select(1); // 1 corresponds to 'Casablanca'
+            Mission.trigger("change");
+          } else {
+            // console.log("Element " + i + " is not visible");
+          }
+        }
+      }
+    }
     function handleOkButtonClick() {
       isOkButtonClicked = true;
 
@@ -309,6 +329,7 @@ const CasaregrPremiumButton = createButton("Casa Regroupement Premium", function
     RemplirCategoryId();
     RemplirLocation();
     RemplirVisaType();
+    RemplirMission();
 
         //--------------------//
     var OnVerifyCaptcha;
